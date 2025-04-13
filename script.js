f36a3205b725200033fec7fab29490fae830b51e
Kamal <temple23062@gmail.com>
2025-04-13 22:29:18 +0400

    ADD:little animations

--- script.js
+++ script.js
@@ -6,3 +6,21 @@ window.addEventListener("scroll", () => {
       header.classList.remove("scrolled");
     }
   });
+
+
+/*Burger MENU*/
+
+document.addEventListener("DOMContentLoaded", function () {
+  const burger = document.querySelector(".burger-menu");
+  const nav = document.querySelector(".universal_nav");
+  const overlay = document.querySelector(".menu-overlay");
+
+  function toggleMenu() {
+    nav.classList.toggle("active");
+    burger.classList.toggle("open");
+    overlay.classList.toggle("active");
+  }
+
+  burger.addEventListener("click", toggleMenu);
+  overlay.addEventListener("click", toggleMenu);
+});
